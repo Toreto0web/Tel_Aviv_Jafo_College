203405852e5f04b35e56c45fb97d801c51e5bd1a
Jonathan Brooker <jonathanbr@mta.ac.il>
2024-09-02 16:31:44 +0300

    a

---
+++ TUI/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academic_College_Tel_Aviv_Jafo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // run program...
+        }
+    }
+
+}
